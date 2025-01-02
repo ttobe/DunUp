0df6e853d4319446e998fb53483977b05badb7d8
ttobe <ahdrmfgur12@gmail.com>
2025-01-03 03:12:31 +0900

    feat: view return으로 변경

--- src/main/java/com/dunup/controller/CharacterController.java
+++ src/main/java/com/dunup/controller/CharacterController.java
@@ -1,35 +1,46 @@
 package com.dunup.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.dunup.dto.CharacterDetailResponseDto;
 import com.dunup.dto.CharacterSearchResponseDto;
 import com.dunup.service.CharacterService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class CharacterController {
 
-  private final CharacterService characterService;
+	private final CharacterService characterService;
 
-  public CharacterController(CharacterService characterService) {
-    this.characterService = characterService;
-  }
+	public CharacterController(CharacterService characterService) {
+		this.characterService = characterService;
+	}
 
-  @GetMapping("/character/search")
-  public CharacterSearchResponseDto searchCharacter(
-      @RequestParam("serverId") String serverId,
-      @RequestParam("characterName") String characterName
-  ) {
-    return characterService.searchCharacter(serverId, characterName);
-  }
+	@GetMapping("/character/search")
+	public String searchCharacter(
+		@RequestParam("serverId") String serverId,
+		@RequestParam("characterName") String characterName,
+		Model model
+	) {
+		CharacterSearchResponseDto responseDto = characterService.searchCharacter(serverId, characterName);
 
-  // 캐릭터 상세 조회 메서드
-  @GetMapping("/character/detail")
-  public CharacterDetailResponseDto getCharacterDetails(
-      @RequestParam("serverId") String serverId,
-      @RequestParam("characterId") String characterId
-  ) {
-    return characterService.getCharacterDetails(serverId, characterId);
-  }
+		model.addAttribute("characters", responseDto.getRows());
+		return "searchResult";
+	}
+
+	@GetMapping("/character/detail")
+	public String getCharacterDetails(
+		@RequestParam("serverId") String serverId,
+		@RequestParam("characterId") String characterId,
+		Model model
+	) {
+		CharacterDetailResponseDto detailResponse = characterService.getCharacterDetails(serverId, characterId);
+
+		// Add character details to the model
+		model.addAttribute("characterDetails", detailResponse);
+
+		return "characterDetail";
+	}
 }
