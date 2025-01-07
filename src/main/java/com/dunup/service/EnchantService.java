86aed2369007b5d0096ee65f614c44d910cb17b6
ttobe <ahdrmfgur12@gmail.com>
2025-01-08 01:01:08 +0900

    feat: 불러와서 지능값 비교 후 낮은 부위 반환

--- src/main/java/com/dunup/service/EnchantService.java
+++ src/main/java/com/dunup/service/EnchantService.java
@@ -8,7 +8,11 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import com.dunup.dto.CharacterDetailResponseDto;
+import com.dunup.dto.EnchantDto;
+import com.dunup.dto.EquipmentDto;
 import com.dunup.dto.MaxEnchantDto;
+import com.dunup.dto.StatusDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -36,4 +40,52 @@ public class EnchantService {
 		enchantData = objectMapper.readValue(resource.getFile(),
 			objectMapper.getTypeFactory().constructCollectionType(List.class, MaxEnchantDto.class));
 	}
+
+	// category와 slotName을 기준으로 enchantData에서 MaxEnchantDto를 찾는 메서드
+	public MaxEnchantDto findEnchantByCategory(String category) throws IOException {
+		for (MaxEnchantDto maxEnchantDto : this.getEnchantData()) {
+			if (maxEnchantDto.getCategory().equals(category)) {
+				return maxEnchantDto;
+			}
+		}
+		return null; // 찾지 못한 경우 null 반환
+	}
+
+	// CharacterDetailResponseDto의 equipment에서 Enchant 비교
+	public String compareEnchantDetails(CharacterDetailResponseDto detailResponse) throws IOException {
+		StringBuilder result = new StringBuilder();
+
+		for (EquipmentDto equipment : detailResponse.getEquipment()) {
+			EnchantDto enchant = equipment.getEnchant(); // EnchantDto
+
+			// TODO: 직업 별 나누기
+			// 일단 지능 기준으로만
+			if (enchant != null) {
+				// 해당 장비자리의 최대 마부 찾기
+				// 0 힘 1 지능 2 체력 3 정신력 4 명성
+				System.out.println(equipment.getSlotName());
+				MaxEnchantDto maxEnchantDto = findEnchantByCategory(equipment.getSlotName());
+				if (maxEnchantDto != null) {
+					StatusDto maxStatus = maxEnchantDto.getEnchant().getStatus().get(1);
+					String maxEnchantIntelligence = maxStatus.getValue();
+					System.out.println("최대 마부 지능 값: " + maxEnchantIntelligence);
+					StatusDto nowStatus = enchant.getStatus().get(1);
+					String nowStatusIntelligence = nowStatus.getValue();
+					System.out.println("현재 마부 지능 값: " + nowStatusIntelligence);
+
+					if (Integer.parseInt(nowStatusIntelligence) < Integer.parseInt(maxEnchantIntelligence)) {
+						result.append(equipment.getSlotName())
+							.append(", ");
+					} else {
+						System.out.println("마부가 낮지 않습니다..");
+					}
+
+				}
+
+			}
+		}
+
+		return result.toString();
+	}
+
 }
